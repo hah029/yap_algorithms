83ee583a40c0f579b76961f0a3d54abeead1f0f8
Roman <roma.chekalin@gmail.com>
2023-04-20 10:07:34 +0300

    Update

--- yap_algorithms_12_sprint.py
+++ yap_algorithms_12_sprint.py
@@ -224,6 +224,154 @@
 #     test()
 # endregion
 
+# region F. Стек - Max
+# class StackMaxEffective:
+#     def __init__(self):
+#         self.items = []
+#         self.max_value = {
+#             'prev': None,
+#             'cur': None
+#         }
+
+#     def push(self, value):
+#         self.items.append(value)
+#         if self.max_value['cur'] is None:
+#             self.max_value['cur'] = value
+#         elif value > self.max_value['cur']:
+#             self.max_value['prev'] = self.max_value['cur']
+#             self.max_value['cur'] = value
+
+#     def pop(self):
+#         if len(self.items) == 0:
+#             print('error')
+#             return
+#         item = self.items.pop()
+#         if len(self.items) == 0:
+#             self.max_value['prev'] = None
+#             self.max_value['cur'] = None
+#         elif item == self.max_value['cur']:
+#             self.max_value[;] = max(self.items)
+#         return item
+
+#     def get_max(self):
+#         return self.max_value['cur']
+
+
+# def read_input():
+#     count = int(input())
+#     input_array = [input().split() for _ in range(count)]
+#     return input_array
+
+
+# def test():
+#     input_array = read_input()
+#     stack = StackMaxEffective()
+
+#     for item in input_array:
+#         if item[0] == 'push':
+#             stack.push(int(item[1]))
+#         elif item[0] == 'pop':
+#             stack.pop()
+#         elif item[0] == 'get_max':
+#             print(stack.get_max())
+
+
+# if __name__ == '__main__':
+#     test()
+# endregion
+
+# region
+
+# cache = {0: 1, 1: 1}
+
+
+# def solution_recursive(num):
+#     if num in cache:
+#         return cache[num]
+#     cache[num] = solution_recursive(num - 1) + solution_recursive(num - 2)
+#     return cache[num]
+
+
+# def solution_iterative(num, k):
+#     if num in [0, 1]:
+#         return 1
+
+#     res = []
+
+#     previous, fib_number = 0, 1
+#     for _ in range(1, num + 1):
+#         previous, fib_number = fib_number, previous + fib_number
+#         res.append(fib_number % pow(10, k))
+
+#     return res
+
+
+def read_input():
+    arr = list(map(int, [*input().split()]))
+    n = arr[0]
+    k = arr[1]
+    return n, k
+
+def pisanoPeriod(m):
+    previous, current = 0, 1
+    for i in range(0, m * m):
+        previous, current = current, (previous + current) % m
+
+        # A Pisano Period starts with 01
+        if (previous == 0 and current == 1):
+            return i + 1
+
+
+# Calculate Fn mod m
+def fibonacciModulo(n, m):
+    # Getting the period
+    pisano_period = pisanoPeriod(m)
+
+    # Taking mod of N with
+    # period length
+    n = n % pisano_period
+
+    previous, current = 0, 1
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    for i in range(n-1):
+        previous, current = current, previous + current
+
+    return (current % m)
+
+
+# Driver Code
+if __name__ == '__main__':
+    print([pisanoPeriod(val) for val in [10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000]])
+    periods = [60, 300, 1500, 15000, 150000, 1500000, 15000000, 150000000]
+    # n, m = read_input()
+    # print(fibonacciModulo(n, 10 ** m))
+
+
+# def test():
+#     n, k = read_input()
+#     periods = []
+#     result = {
+#         1: 60,
+#         2: 300,
+#         3: 1500,
+#         4: 15000,
+#         5: 15000,
+#         6: 15000,
+#         7: 15000,
+#         8: 15000,
+#     }
+#     # for i in range(n):
+#     #     result.append(solution_iterative(i, k) % pow(10, k))
+#     print(result)
+
+
+# if __name__ == '__main__':
+#     test()
+# endregion
+
 
 # endregion
 # ===============================================================================================
---
+++ yap_algorithms_13_sprint.py
@@ -0,0 +1,187 @@
+
+# ===============================================================================================
+# region Примеры из теории
+
+# region Вариант нахождения скользящего среднего для временного ряда
+# endregion
+
+# endregion
+# ===============================================================================================
+
+# ===============================================================================================
+# region Задачи Яндекс-контест
+
+# region L. Два велосипеда (id=86059152)
+# import sys
+
+
+# def binarySearch(array, left, right, value):
+#     if right <= left:
+#         if array[right] < value:
+#             return -1
+#         else:
+#             return right
+#     mid = (left + right) // 2
+#     if array[mid] == value:
+#         if array[mid - 1] == value:
+#             return binarySearch(array, left, mid, value)
+#         else:
+#             return mid
+#     elif array[mid] > value:
+#         return binarySearch(array, left, mid, value)
+#     else:
+#         return binarySearch(array, mid + 1, right, value)
+
+
+# def solution(array, value):
+#     first_day = binarySearch(array, 0, len(array)-1, value)
+#     second_day = binarySearch(array, first_day, len(array)-1, value * 2)
+#     first_day = first_day if first_day == -1 else first_day + 1
+#     second_day = second_day if second_day == -1 else second_day + 1
+#     return first_day, second_day
+
+
+# def read_input():
+#     _ = int(input())
+#     input_array = list(map(int, sys.stdin.readline().rstrip().split()))
+#     value = int(input())
+#     return input_array, value
+
+
+# if __name__ == '__main__':
+#     array, value = read_input()
+#     print(*solution(array, value))
+# endregion
+
+# region A. Генератор скобок (id=86060342)
+# def correct_sequence(prefix):
+#     tmp_list = []
+#     for ch in prefix:
+#         if ch == '(':
+#             tmp_list.append(ch)
+#         elif ch == ')':
+#             if len(tmp_list) == 0:
+#                 return False
+#             tmp_list.pop()
+#     if len(tmp_list) == 0:
+#         return True
+#     else:
+#         return False
+
+
+# def gen_binary(n, prefix):
+#     if n == 0:
+#         if correct_sequence(prefix):
+#             print(prefix)
+#     else:
+
+#         gen_binary(n - 1, prefix + '(')
+#         gen_binary(n - 1, prefix + ')')
+
+
+# def solution(value):
+#     gen_binary(value - 1, '(')
+
+
+# def read_input():
+#     value = int(input())
+#     return value
+
+
+# if __name__ == '__main__':
+#     value = read_input()
+#     solution(value * 2)
+# endregion
+
+# region B. Комбинации (id=86061875)
+# def gen_binary(n, value_dict, result, prefix):
+#     if n == 0:
+#         result.append(prefix)
+#         return
+#     else:
+#         for index in range(len(value_dict)):
+#             for char in value_dict[index]:
+#                 gen_binary(n - 1, value_dict[index + 1:], result, prefix + char)
+#     return result
+
+
+# def solution(array):
+#     my_dict = {
+#         '2': 'abc',
+#         '3': 'def',
+#         '4': 'ghi',
+#         '5': 'jkl',
+#         '6': 'mno',
+#         '7': 'pqrs',
+#         '8': 'tuv',
+#         '9': 'wxyz'
+#     }
+#     value_dict = [my_dict[ch] for ch in array]
+#     result = gen_binary(len(value_dict), value_dict, [], '')
+#     return result
+
+
+# def read_input():
+#     value = [*input()]
+#     return value
+
+
+# if __name__ == '__main__':
+#     value = read_input()
+#     print(*solution(value))
+# endregion
+
+# region J. Пузырёк (id=?)
+import sys
+
+
+def solution(array, direction='asc'):
+    j = 0
+    flag = True
+    while j < len(array):
+        i = 0
+        tmp = None
+        while i < len(array) - j - 1:
+            if array[i] > array[i + 1]:
+                tmp = array[i + 1]
+                array[i + 1] = array[i]
+                array[i] = tmp
+                flag = True
+            i += 1
+        if flag:
+            print(*array)
+            flag = False
+        j += 1
+
+
+def read_input():
+    _ = input()
+    array = list(map(int, sys.stdin.readline().rstrip().split()))
+    try:
+        _ = input()
+    except:
+        _ = None
+    return array
+
+
+if __name__ == '__main__':
+    array = read_input()
+    # read_input()
+    # print(array)
+    solution(array)
+    # print(*solution(array), sep='')
+# endregion
+
+
+# endregion
+# ===============================================================================================
+
+# ===============================================================================================
+# region Финальные задания (тоже Яндекс-контест)
+
+# region Задача A: Ближайший ноль (id=84157187)
+
+# endregion
+
+# endregion
+# ===============================================================================================
